9a2ea450dd443e0c88a4a22bfe4e8577aba153d9
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2020-04-14 19:28:17 -0400

    Displays header

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {useHeaderStyles} from './classes'
+
+const Header = () => {
+  const classes = useHeaderStyles()
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            JupiterOne Questions
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default Header
