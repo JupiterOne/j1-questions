60e73d2d368288bf87416204b06f18e8fdeac980
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2020-04-20 16:53:11 -0400

    Refactoring of filter method

--- src/methods/filterQuestions.ts
+++ src/methods/filterQuestions.ts
@@ -1,7 +1,7 @@
 import {Question} from '../types'
 // import fuzzy from 'fuzzy';
-// import { test } from 'fuzzyjs'
-import matchSorter from 'match-sorter'
+import { test } from 'fuzzyjs'
+// import matchSorter from 'match-sorter'
 
 // interface FilterOptions {
 //   searchText: string;
@@ -9,20 +9,40 @@ import matchSorter from 'match-sorter'
 //   tags: string[];
 // }
 
-const filteredQuestions = (questions: Question[], integration: string, tags: string[], search: string, questionNumber: number) => (
-  matchSorter(
-  questions
-    .slice(0, !(integration !== 'none' || tags.length !== 0 || search !== '') ? questionNumber : questions.length)
-    .filter((question: Question) => integration !== 'none' ? question.integration === integration : true)
-    .filter((question: Question) => {
-      const array : boolean[] = []
-      for (let key of tags) {
-        array.push(question.tags !== undefined ? question.tags.includes(key) : false)
-      }
-      return !array.includes(false)
-    })
-  ,search, {keys: ['title', 'description', 'tags']})
-)
+// NOTE: Christian helped me out with optimizing this function.
+const doesMatchTags = (question: Question, tags: string[]): boolean => {
+  // Return true if no tags are being filtered
+  if (!question.tags) {
+    return tags.length === 0
+  }
+
+  for (let tag of tags) {
+    if (!question.tags.includes(tag)) {
+      return false
+    }
+  }
+
+  return true
+}
+
+// NOTE: Christian helped me out with optimizing this function.
+const filteredQuestions = (questions: Question[], integration: string, tags: string[], search: string, questionNumber: number) => {
+  const isFiltering = integration === 'none' || tags.length === 0 || search === '';
+
+  const results = questions.filter(question => {
+    const matchesIntegration = integration === 'none' || question.integration === integration
+    const matchesTags = doesMatchTags(question, tags)
+    const matchesSearch = test(search, `${question.title} ${question.description}`)
+
+    return matchesIntegration && matchesTags && matchesSearch
+  })
+
+  if (!isFiltering) {
+    return results.slice(0, questionNumber)
+  }
+
+  return results
+}
 
 
 export default filteredQuestions;
