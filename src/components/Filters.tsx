b7f8314eed6e55b77db09b139088faca226a320c
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2020-04-27 08:54:49 -0400

    Context update

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, { useContext } from "react";
 import {
   Paper,
   Icon,
@@ -11,15 +11,15 @@ import {
   Hidden,
   Checkbox,
   Zoom
-} from '@material-ui/core'
-import {useFilterStyles} from '../classes'
-import DoneIcon from '@material-ui/icons/Done';
-import TagIcon from '@material-ui/icons/LocalOfferOutlined';
-import IntegrationIcon from '@material-ui/icons/BuildOutlined';
-import FilterListIcon from '@material-ui/icons/FilterListRounded';
-import CategoryIcon from '@material-ui/icons/CategoryOutlined';
+} from "@material-ui/core";
+import { useFilterStyles } from "../classes";
+import DoneIcon from "@material-ui/icons/Done";
+import TagIcon from "@material-ui/icons/LocalOfferOutlined";
+import IntegrationIcon from "@material-ui/icons/BuildOutlined";
+import FilterListIcon from "@material-ui/icons/FilterListRounded";
+import CategoryIcon from "@material-ui/icons/CategoryOutlined";
 import { useWindowSize } from "@reach/window-size";
-import Context from '../AppContext'
+import Context from "../AppContext";
 
 interface Props {
   // managedQuestions: ManagedQuestionJSON;
@@ -36,91 +36,129 @@ interface Props {
 }
 
 const Filters = (props: Props) => {
-  console.log('Filter')
-  const {allCategories, categories, managedQuestions, integrations, tagFilter, allTags, tags, setFilterLogic} = useContext(Context)
+  console.log("Filter");
+  const {
+    allCategories,
+    categories,
+    managedQuestions,
+    integrations,
+    tagFilter,
+    allTags,
+    tags,
+    setFilterLogic
+  } = useContext(Context);
 
-  const classes = useFilterStyles()
-  const windowSize = useWindowSize()
+  const classes = useFilterStyles();
+  const windowSize = useWindowSize();
 
   return (
     <Hidden>
-      <Paper elevation={0} className={windowSize.width > 750 ? classes.root : classes.smallRoot}>
+      <Paper
+        elevation={0}
+        className={windowSize.width > 750 ? classes.root : classes.smallRoot}
+      >
         <Box m={2} className={classes.section}>
           <Icon>
-            <FilterListIcon/>
+            <FilterListIcon />
           </Icon>
-          <Typography variant='h6'>Filters</Typography>
+          <Typography variant="h6">Filters</Typography>
         </Box>
         <Box m={2} className={`${classes.section} ${classes.flexWrap}`}>
           <Icon>
-            <CategoryIcon/>
+            <CategoryIcon />
           </Icon>
-          <Typography variant='subtitle1'>Category</Typography>
+          <Typography variant="subtitle1">Category</Typography>
         </Box>
         <Box m={2}>
-          {allCategories.map((category: any) =>
-            <div onClick={() => props.setCategories({category})} key={category}>
+          {allCategories.map((category: any) => (
+            <div
+              onClick={() => props.setCategories({ category })}
+              key={category}
+            >
               <Box>
                 <Checkbox
-                  edge='start'
+                  edge="start"
                   name={category}
                   checked={categories.includes(category)}
-                  onChange={() => props.setCategories({category})}
+                  onChange={() => props.setCategories({ category })}
                 />
                 {category}
               </Box>
-            </div>
-          )}
-        </Box>
-        <Box m={2} className={classes.section}>
-          <Icon>
-            <IntegrationIcon/>
-          </Icon>
-          <Typography variant='subtitle1'>Integrations</Typography>
-        </Box>
-        <Box m={0.7}>
-          {[...Object.keys(managedQuestions.integrations), 'none'].map((integration: string, index: number) => (
-            <div key={index}>
-              <Checkbox
-                checked={integrations.includes(integration)}
-                onChange={() => props.integrationClicked(integration)}
-              />
-              {integration /* {Object.keys(managedQuestions.integrations).length > 0 && integration !== 'none' ? managedQuestions.integrations[integration].title : 'none'} */}
             </div>
           ))}
         </Box>
         <Box m={2} className={classes.section}>
           <Icon>
-            <TagIcon/>
+            <IntegrationIcon />
           </Icon>
-          <Typography variant='subtitle1'>Tags</Typography>
+          <Typography variant="subtitle1">Integrations</Typography>
+        </Box>
+        <Box m={0.7}>
+          {[...Object.keys(managedQuestions.integrations), "none"].map(
+            (integration: string, index: number) => (
+              <div key={index}>
+                <Checkbox
+                  checked={integrations.includes(integration)}
+                  onChange={() => props.integrationClicked(integration)}
+                />
+                {Object.keys(managedQuestions.integrations).length > 0 &&
+                integration !== "none"
+                  ? managedQuestions.integrations[integration].title
+                  : "none"}
+              </div>
+            )
+          )}
+        </Box>
+        <Box m={2} className={classes.section}>
+          <Icon>
+            <TagIcon />
+          </Icon>
+          <Typography variant="subtitle1">Tags</Typography>
         </Box>
         <Box m={2}>
           <ButtonGroup>
-            <Button color={(tagFilter === 'all') ? 'primary' : 'default'} onClick={() => setFilterLogic('all')}>Filter by all</Button>
-            <Button color={(tagFilter === 'any') ? 'primary' : 'default'} onClick={() => setFilterLogic('any')}>Filter by any</Button>
+            <Button
+              color={tagFilter === "all" ? "primary" : "default"}
+              onClick={() => setFilterLogic("all")}
+            >
+              Filter by all
+            </Button>
+            <Button
+              color={tagFilter === "any" ? "primary" : "default"}
+              onClick={() => setFilterLogic("any")}
+            >
+              Filter by any
+            </Button>
           </ButtonGroup>
         </Box>
         <Box m={2} className={`${classes.section} ${classes.flexWrap}`}>
           <Box>
-            {allTags
-              .sort()
-              .map((tag: string, index : number) => (
-                <Chip
-                  color='primary'
-                  variant='outlined'
-                  avatar={tags.includes(tag) ? <Zoom in={tags.includes(tag)}><Avatar><DoneIcon /></Avatar></Zoom> : undefined}
-                  className={classes.tag}
-                  key={index}
-                  onClick={() => props.tagCheckClicked(tag)}
-                  label={tag}
-                />
+            {allTags.sort().map((tag: string, index: number) => (
+              <Chip
+                color="primary"
+                variant="outlined"
+                avatar={
+                  tags.includes(tag) ? (
+                    <Zoom in={tags.includes(tag)}>
+                      <Avatar>
+                        <DoneIcon />
+                      </Avatar>
+                    </Zoom>
+                  ) : (
+                    undefined
+                  )
+                }
+                className={classes.tag}
+                key={index}
+                onClick={() => props.tagCheckClicked(tag)}
+                label={tag}
+              />
             ))}
           </Box>
         </Box>
       </Paper>
     </Hidden>
-  )
-}
+  );
+};
 
 export default React.memo(Filters);
