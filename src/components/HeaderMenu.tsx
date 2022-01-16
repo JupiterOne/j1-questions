dc830d8de9cc83ce3dc9f1f61e181e0e4990aa1a
Tim Neighbors <tim@bigorangelab.com>
2022-01-16 15:10:22 -0600

    Update HeaderMenu.tsx
    Updated menu links - @hasard1 - Make sure to pull latest

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 const HeaderMenu = () => {
   const classes = useHeaderStyles();
   const menuItem = [
-    {title: "Community", url: "/",type: "link",isActive: false},
-    {title: "Questions Library", url: "/",type: "link", isActive: true},
-    {title: "Documentation", url: "/",type: "link",isActive: false},
-    {title: "Blog", url: "/",type: "link",isActive: false},
-    {title: "Events", url: "/",type: "link",isActive: false},
+    {title: "Community", url: "https://community.askj1.com",type: "link",isActive: false},
+    {title: "Questions Library", url: "https://ask.us.jupiterone.io/",type: "link", isActive: true},
+    {title: "Documentation", url: "https://community.askj1.com/kb/docs",type: "link",isActive: false},
+    {title: "Blog", url: "https://try.jupiterone.com/",type: "link",isActive: false},
+    {title: "Events", url: "https://try.jupiterone.com/events",type: "link",isActive: false},
     {title: "Swag", url: "/",type: "link",isActive: false},
     {title: "Divider", type: "divider",isActive: false},
-    {title: "Github", url: "/",type: "button",isActive: false},
-    {title: "Slack", url: "/",type: "button",isActive: false},
-    {title: "JupiterOne", url: "/",type: "button",isActive: false},
+    {title: "Github", url: "https://github.com/JupiterOne",type: "button",isActive: false},
+    {title: "Slack", url: "https://join.slack.com/t/jupiterone-community/shared_invite/zt-9b0a2htx-m8PmSWMbkjqCzF2dIZiabw",type: "button",isActive: false},
+    {title: "JupiterOne", url: "https://jupiterone.com/",type: "button",isActive: false},
   ]
   return (
     <div className="ask-j1-header_menu">
